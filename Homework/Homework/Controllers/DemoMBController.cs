ce9f9ef794fd0b7afd35589e33335b3ac043b4f3
Houston <houston0625@hotmail.com>
2017-05-12 17:43:57 +0800

    2017/05/12

---
+++ Homework/Homework/Controllers/DemoMBController.cs
@@ -0,0 +1,35 @@
+﻿using Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class DemoMBController : Controller
+    {
+        // GET: DemoMB
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DemoMB()
+        {
+            DemoMBViewModel model = new DemoMBViewModel();
+
+            model.Value_One = "Key";
+            model.Value_Two = "Hidden";
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult DemoMB(DemoMBViewModel model)
+        {
+
+            return View(model);
+        }
+    }
+}
---
+++ Homework/Homework/Models/ViewModels/DemoMBViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework.Models
+{
+    public class DemoMBViewModel
+    {
+        public String Value_One { get; set; }
+
+        public String Value_Two { get; set; }
+        public String Value_Three { get; set; }
+        public String Value_Four { get; set; }
+    }
+}
